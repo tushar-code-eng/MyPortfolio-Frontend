482d819dd0e0f443caa347940792ebc3ace747c7
Tushar <tusharbajaj10@gmail.com>
2024-12-23 02:05:45 +0530

    very small bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 
 import svgToDataUri from "mini-svg-data-uri";
 
-const colors = require("tailwindcss/colors");
+import colors from "tailwindcss/colors";
+
 const {
     default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
